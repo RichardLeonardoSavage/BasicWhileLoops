87aecb720f844b285185a6ee75a6c39ac72cccf2
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 11:59:04 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BasicWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //basic while loop
+            int index = 1;
+            while(index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+
+            //basic do while loop
+            int indexTwo = 20;
+            do
+            {
+                Console.WriteLine(indexTwo);
+                indexTwo--;
+            } while(indexTwo >= 0);
+
+            Console.ReadLine();
+
+        }
+    }
+}
